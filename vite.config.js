09f67d781de641f67b15936142512e99a2b0a460
Mahendra Nikam <info@mahendratechnosoft.com>
2025-11-06 11:12:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174, // 👈 change the port number here
+    open: true, // optional – auto open browser
+  },
 })
