d4ca82e52d16ab6cac6ff77ddfff8f420f6da908
adityaslohar <adityalohar5838@gmail.com>
2025-11-10 18:15:22 +0530

    icon changes in slide bar

--- src/Components/Pages/Employee/SidebarEmployee.jsx
+++ src/Components/Pages/Employee/SidebarEmployee.jsx
@@ -10,7 +10,7 @@ function SidebarEmployee({ isOpen, toggleSidebar, onSwitchToLogin }) {
       name: "Lead",
       path: "/Employee/LeadList",
       icon: (
-        <svg
+         <svg
           className="w-5 h-5"
           fill="none"
           stroke="currentColor"
@@ -20,7 +20,13 @@ function SidebarEmployee({ isOpen, toggleSidebar, onSwitchToLogin }) {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth={2}
-            d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
           />
         </svg>
       ),
@@ -30,7 +36,7 @@ function SidebarEmployee({ isOpen, toggleSidebar, onSwitchToLogin }) {
       name: "Customer",
       path: "/Employee/CustomerList",
       icon: (
-        <svg
+       <svg
           className="w-5 h-5"
           fill="none"
           stroke="currentColor"
@@ -110,18 +116,17 @@ function SidebarEmployee({ isOpen, toggleSidebar, onSwitchToLogin }) {
       name: "Proposal",
       path: "/Employee/Proposal",
       icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
+           <svg
+          className="w-5 h-5"
           fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
           stroke="currentColor"
-          class="size-6"
+          viewBox="0 0 24 24"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
           />
         </svg>
       ),
